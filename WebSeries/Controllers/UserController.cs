a788e6d8e69e8ef688f14dfe4c7e1ba55c65a771
Junayed Kabir Fahim <junayedkabirfahim@gmail.com>
2022-07-13 21:57:38 +0600

    user-registration-validation

---
+++ WebSeries/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebSeries.EntityFramework;
+
+namespace WebSeries.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registration(User u)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("User/Registration");
+            }
+            return View(u);
+        }
+    }
+}
