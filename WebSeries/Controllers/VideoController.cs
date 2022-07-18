64ba598053493e798d4d02757a0bcfa3136a3784
F1roz <feroz.fahim.9@gmail.com>
2022-07-18 22:22:19 +0600

    upload file implemented with database

--- WebSeries/Controllers/VideoController.cs
+++ WebSeries/Controllers/VideoController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Validation;
 using System.IO;
 using System.Linq;
 using System.Web;
@@ -23,8 +24,45 @@ namespace WebSeries.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult Upload(HttpPostedFileBase file)
+        public ActionResult Upload(VideoFile v)
         {
+
+            try{ 
+
+            using (WebSeriesDBEntities db = new WebSeriesDBEntities())
+            {
+
+                Video video = new Video();
+                video.VideoTitle = v.VideoTitle;
+                video.Description = v.Description;
+                video.Status = "Ap";
+                video.UserId = 2;
+                    video.UploadDate = DateTime.Now;
+                video.VideoPath = SaveToPhysicalLocation(v.VideoPath);
+                db.Videos.Add(video);
+                db.SaveChanges();
+
+            }
+            ModelState.Clear();
+            ViewBag.Message = SaveToPhysicalLocation(v.VideoPath).Length;
+            //ViewBag.Message =video;
+            return View();
+        }
+            catch (DbEntityValidationException e)
+            {
+                foreach (var eve in e.EntityValidationErrors)
+                {
+                    ViewBag.Message = "Entity of type \"{0}\" in state \"{1}\" has the following validation errors:" +
+                        eve.Entry.Entity.GetType().Name+ eve.Entry.State;
+                    foreach (var ve in eve.ValidationErrors)
+                    {
+                        ViewBag.Message= "- Property: \"{0}\", Error: \"{1}\""+ ve.PropertyName+ ve.ErrorMessage;
+                    }
+                }
+                return View();
+            }
+            /*string file = v.VideoPath;
+            string FileName = Path.GetFileNameWithoutExtension(v.ImageFile.FileName);*/
             /* // Verify that the user selected a file
              if (file != null && file.ContentLength > 0)
              {
@@ -36,7 +74,7 @@ namespace WebSeries.Controllers
              }
              // redirect back to the index action to show the form once again
              return RedirectToAction("Upload");*/
-            try
+            /*try
             {
                 if (file.ContentLength > 0)
                 {
@@ -52,7 +90,18 @@ namespace WebSeries.Controllers
             {
                 ViewBag.Message = "File upload failed!!";
                 return View();
+            }*/
+        }
+        private string SaveToPhysicalLocation(HttpPostedFileBase file)
+        {
+            if (file.ContentLength > 0)
+            {
+                var fileName = Path.GetFileName(file.FileName);
+                var path = Path.Combine(Server.MapPath("~/FTP/"), fileName);
+                file.SaveAs(path);
+                return path;
             }
+            return string.Empty;
         }
     }
 }
---
+++ WebSeries/EntityFramework/VideoFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebSeries.EntityFramework
+{
+    public class VideoFile
+    {
+        public string VideoTitle { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public int UserId { get; set; }
+        public HttpPostedFileBase VideoPath { get; set; }
+    }
+}
